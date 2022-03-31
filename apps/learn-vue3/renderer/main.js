f0c0478b367c625750f8b4ddccee582ba32a58db
daolanfler <daolanfler@gmail.com>
2022-03-31 10:45:01 +0800

    feat(learn-vue3): 8.9 patchElement

--- apps/learn-vue3/renderer/main.js
+++ apps/learn-vue3/renderer/main.js
@@ -15,6 +15,50 @@ import { normalizeClass } from "./shared";
 
 function createRenderer(options) {
   const { createElement, insert, setElementText, patchProps } = options;
+  function patchElement(n1, n2, ) {
+    const el = (n2.el = n1.el);
+    const oldProps = n1.props;
+    const newProps = n2.props;
+
+    for (const key in newProps) {
+      if (newProps[key] !== oldProps[key]) {
+        patchProps(el, key, oldProps[key], newProps[key]);
+      }
+    }
+
+    for (const key in oldProps) {
+      if (!(key in newProps)) {
+        patchProps(el, key, oldProps[key], null);
+      }
+    }
+
+    patchChildren(n1, n2, el);
+  }
+
+  function patchChildren(n1, n2, container) {
+    if (typeof n2.children === "string") {
+      if (Array.isArray(n1.children)) {
+        n1.children.forEach((c) => unmount(c));
+      }
+
+      setElementText(container, n2.children);
+    } else if (Array.isArray(n2.children)) {
+      if (Array.isArray(n1.children)) {
+        // 核心 diff 算法
+        n1.children.forEach(c => unmount(c));
+        n2.children.forEach(c => patch(null, c, container));
+      } else {
+        setElementText(container, '');
+        n2.children.forEach(c => patch(null, c, container));
+      }
+    } else {
+      if (Array.isArray(n1.children)) {
+        n1.children.forEach(c => unmount(c));
+      } else if (typeof n1.children === 'string') {
+        setElementText(container, '');
+      }
+    }
+  }
 
   function patch(n1, n2, container) {
     if (n1 && n1.type !== n2.type) {
@@ -27,7 +71,7 @@ function createRenderer(options) {
       if (!n1) {
         mountElement(n2, container);
       } else {
-        // patchElement(n1, n2)
+        patchElement(n1, n2);
       }
     } else if (typeof type === "object") {
       // 组件
@@ -39,11 +83,6 @@ function createRenderer(options) {
   function mountElement(vnode, container) {
     // el 引用真实的 DOM 元素
     const el = (vnode.el = createElement(vnode.type));
-    if (vnode.props) {
-      for (const key in vnode.props) {
-        patchProps(el, key, null, vnode.props[key]);
-      }
-    }
     if (typeof vnode.children === "string") {
       setElementText(el, vnode.children);
     } else if (Array.isArray(vnode.children)) {
@@ -51,6 +90,13 @@ function createRenderer(options) {
         patch(null, child, el);
       });
     }
+
+    if (vnode.props) {
+      for (const key in vnode.props) {
+        patchProps(el, key, null, vnode.props[key]);
+      }
+    }
+
     insert(el, container);
   }
 
@@ -77,21 +123,40 @@ function createRenderer(options) {
 
 const renderer = createRenderer({
   createElement(tag) {
-    console.log(`createElement: ${tag}`);
-    return { tag };
+    return document.createElement(tag);
   },
   setElementText(el, text) {
-    console.log(`setElementText: ${text}`);
-    el.text = text;
+    el.innerText = text;
   },
   insert(el, parent, anchor = null) {
-    console.log(`insert: ${el.tag}`);
-    console.log(parent);
-    parent.children = el;
+    parent.appendChild(el);
   },
   patchProps(el, key, preValue, nextValue) {
     // Difference between DOM Properties and HTML Attributes
-    if (key === "class") {
+    if (/^on/.test(key)) {
+      const invokers = el._vei || (el._vei = {});
+      let invoker = invokers[key];
+      const name = key.slice(2).toLowerCase();
+      if (nextValue) {
+        if (!invoker) {
+          invoker = el._vei[key] = (e) => {
+            // 事件触发时间早于函数绑定时间，则不执行事件处理函数
+            if (e.timestamp < invoker.attached) return;
+            if (Array.isArray(invoker.value)) {
+              invoker.value.forEach((fn) => fn(e));
+            } else {
+              invoker.value(e);
+            }
+          };
+          invoker.value = nextValue;
+          // 时间处理函数被绑定的时间
+          invoker.attached = performance.now();
+          el.addEventListener(name, invoker);
+        } else if (invoker) {
+          el.removeEventListener(name, invoker);
+        }
+      }
+    } else if (key === "class") {
       el.className = normalizeClass(nextValue) || "";
     } else if (shouldSetAsProps(el, key, nextValue)) {
       const type = typeof el[key];
@@ -118,9 +183,37 @@ const vnode = {
       bar: true,
     },
   },
-  children: "Hello World",
+  children: "Hello World How",
 };
 
-const container = { type: "root" };
+// const container = { type: "root" };
 
-renderer.render(vnode, container);
+// renderer.render(vnode, document.querySelector("#app"));
+
+const bol = ref(false);
+
+effect(() => {
+  const vnode = {
+    type: "div",
+    props: bol.value
+      ? {
+        onClick: () => {
+          alert("父元素 clicked");
+        },
+      }
+      : {},
+    children: [
+      {
+        type: "p",
+        props: {
+          onClick: () => {
+            bol.value = true;
+          },
+        },
+        children: "text",
+      },
+    ],
+  };
+  console.log("effect", bol.value);
+  renderer.render(vnode, document.querySelector("#app"));
+});
