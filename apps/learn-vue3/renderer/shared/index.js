f3b1007eed6bb166f0e294190c8419c467c2f021
daolanfler <daolanfler@gmail.com>
2022-04-03 21:21:18 +0800

    feat(learn-vue3): 快速diff 算法

--- apps/learn-vue3/renderer/shared/index.js
+++ apps/learn-vue3/renderer/shared/index.js
@@ -1,25 +1,69 @@
-export const isString = val => typeof val === 'string';
-export const isObject = val => typeof val === 'object' && val !== null;
-export const isArray = val => Array.isArray(val);
+export const isString = (val) => typeof val === "string";
+export const isObject = (val) => typeof val === "object" && val !== null;
+export const isArray = (val) => Array.isArray(val);
 
-
-export function normalizeClass(value){
-  let res = '';
+export function normalizeClass(value) {
+  let res = "";
   if (isString(value)) {
     res = value;
   } else if (isArray(value)) {
     for (let i = 0; i < value.length; i++) {
       const normalized = normalizeClass(value[i]);
       if (normalized) {
-        res += normalized + ' ';
+        res += normalized + " ";
       }
     }
   } else if (isObject(value)) {
     for (const name in value) {
       if (value[name]) {
-        res += name + ' ';
+        res += name + " ";
       }
     }
   }
   return res.trim();
+}
+// [2,3,1,-1]
+
+export function getSequence(arr) {
+  const p = arr.slice();
+  const result = [0];
+  let i, j, u, v, c;
+
+  const len = arr.length;
+  for (i = 0; i < len; i++) {
+    const arrI = arr[i];
+    if (arrI !== 0) {
+      j = result[result.length - 1];
+      if (arr[j] < arrI) {
+        p[i] = j;
+        result.push(i);
+        continue;
+      }
+      u = 0;
+      v = result.length - 1;
+      while (u < v) {
+        c = ((u + v) / 2) | 0;
+        if (arr[result[c]] < arrI) {
+          u = c + 1;
+        } else {
+          v = c;
+        }
+      }
+
+      if (arrI < arr[result[u]]) {
+        if (u > 0) {
+          p[i] = result[u - 1];
+        }
+        result[u] = i;
+      }
+    }
+  }
+
+  u = result.length;
+  v = result[u - 1];
+  while (u-- > 0) {
+    result[u] = v;
+    v = p[v];
+  }
+  return result;
 }
