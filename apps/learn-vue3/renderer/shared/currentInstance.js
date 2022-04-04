3ab814bcd77070ebae76d7115207a7d6b39183ba
daolanfler <daolanfler@gmail.com>
2022-04-04 23:52:44 +0800

    feat(learn-vue3): 12.8 注册生命周期

---
+++ apps/learn-vue3/renderer/shared/currentInstance.js
@@ -0,0 +1,14 @@
+export let currentInstace = null;
+
+export function setCurrentInstance(instance) {
+  currentInstace = instance;
+}
+
+export function onMounted(fn) {
+  if (currentInstace) {
+    currentInstace.mounted.push(fn)
+  } else {
+    console.error('onMounted 只能在 setup 中调用')
+  }
+
+}
