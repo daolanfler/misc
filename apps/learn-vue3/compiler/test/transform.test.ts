080106fc0f783b16a4c5a33dc55a72fe5787b5b0
daolanfler <daolanfler@gmail.com>
2022-04-09 17:27:47 +0800

    feat(learn-vue3): 15.5 transform to js ast

--- apps/learn-vue3/compiler/test/transform.test.ts
+++ apps/learn-vue3/compiler/test/transform.test.ts
@@ -1,5 +1,5 @@
-import { expect, it, test } from 'vitest';
-import { parse, tokenize } from '../parse';
+import { expect, it } from 'vitest';
+import { parse } from '../parse';
 import { transform } from '../transform';
 
 
@@ -7,10 +7,225 @@ it('shourd transform to javascript ast', () => {
   const template = '<div><p>Vue</p><p>Template</p></div>'
   const ast = parse(template)
   expect(transform(ast)).toMatchInlineSnapshot(`
-    "Root: 
-    --Element: div
-    ----Element: h1
-    ----Element: h1
-    "
+    {
+      "children": [
+        {
+          "children": [
+            {
+              "children": [
+                {
+                  "content": "Vue",
+                  "jsNode": {
+                    "type": "StringLiteral",
+                    "value": "Vue",
+                  },
+                  "type": "Text",
+                },
+              ],
+              "jsNode": {
+                "arguments": [
+                  {
+                    "type": "StringLiteral",
+                    "value": "p",
+                  },
+                  {
+                    "elements": [
+                      {
+                        "type": "StringLiteral",
+                        "value": "Vue",
+                      },
+                    ],
+                    "type": "ArrayExpression",
+                  },
+                ],
+                "callee": {
+                  "name": "h",
+                  "type": "Identifier",
+                },
+                "type": "CallExpression",
+              },
+              "tag": "p",
+              "type": "Element",
+            },
+            {
+              "children": [
+                {
+                  "content": "Template",
+                  "jsNode": {
+                    "type": "StringLiteral",
+                    "value": "Template",
+                  },
+                  "type": "Text",
+                },
+              ],
+              "jsNode": {
+                "arguments": [
+                  {
+                    "type": "StringLiteral",
+                    "value": "p",
+                  },
+                  {
+                    "elements": [
+                      {
+                        "type": "StringLiteral",
+                        "value": "Template",
+                      },
+                    ],
+                    "type": "ArrayExpression",
+                  },
+                ],
+                "callee": {
+                  "name": "h",
+                  "type": "Identifier",
+                },
+                "type": "CallExpression",
+              },
+              "tag": "p",
+              "type": "Element",
+            },
+          ],
+          "jsNode": {
+            "arguments": [
+              {
+                "type": "StringLiteral",
+                "value": "div",
+              },
+              {
+                "elements": [
+                  {
+                    "arguments": [
+                      {
+                        "type": "StringLiteral",
+                        "value": "p",
+                      },
+                      {
+                        "elements": [
+                          {
+                            "type": "StringLiteral",
+                            "value": "Vue",
+                          },
+                        ],
+                        "type": "ArrayExpression",
+                      },
+                    ],
+                    "callee": {
+                      "name": "h",
+                      "type": "Identifier",
+                    },
+                    "type": "CallExpression",
+                  },
+                  {
+                    "arguments": [
+                      {
+                        "type": "StringLiteral",
+                        "value": "p",
+                      },
+                      {
+                        "elements": [
+                          {
+                            "type": "StringLiteral",
+                            "value": "Template",
+                          },
+                        ],
+                        "type": "ArrayExpression",
+                      },
+                    ],
+                    "callee": {
+                      "name": "h",
+                      "type": "Identifier",
+                    },
+                    "type": "CallExpression",
+                  },
+                ],
+                "type": "ArrayExpression",
+              },
+            ],
+            "callee": {
+              "name": "h",
+              "type": "Identifier",
+            },
+            "type": "CallExpression",
+          },
+          "tag": "div",
+          "type": "Element",
+        },
+      ],
+      "jsNode": {
+        "body": [
+          {
+            "return": {
+              "arguments": [
+                {
+                  "type": "StringLiteral",
+                  "value": "div",
+                },
+                {
+                  "elements": [
+                    {
+                      "arguments": [
+                        {
+                          "type": "StringLiteral",
+                          "value": "p",
+                        },
+                        {
+                          "elements": [
+                            {
+                              "type": "StringLiteral",
+                              "value": "Vue",
+                            },
+                          ],
+                          "type": "ArrayExpression",
+                        },
+                      ],
+                      "callee": {
+                        "name": "h",
+                        "type": "Identifier",
+                      },
+                      "type": "CallExpression",
+                    },
+                    {
+                      "arguments": [
+                        {
+                          "type": "StringLiteral",
+                          "value": "p",
+                        },
+                        {
+                          "elements": [
+                            {
+                              "type": "StringLiteral",
+                              "value": "Template",
+                            },
+                          ],
+                          "type": "ArrayExpression",
+                        },
+                      ],
+                      "callee": {
+                        "name": "h",
+                        "type": "Identifier",
+                      },
+                      "type": "CallExpression",
+                    },
+                  ],
+                  "type": "ArrayExpression",
+                },
+              ],
+              "callee": {
+                "name": "h",
+                "type": "Identifier",
+              },
+              "type": "CallExpression",
+            },
+            "type": "ReturnStatement",
+          },
+        ],
+        "id": {
+          "name": "render",
+          "type": "Identifier",
+        },
+        "params": [],
+        "type": "FunctionDecl",
+      },
+      "type": "Root",
+    }
   `);
 });
--- apps/learn-vue3/compiler/transform.js
+++ apps/learn-vue3/compiler/transform.js
@@ -1,90 +1,133 @@
 import { parse } from "./parse.js";
 
-function dump(node, indent = 0, context = {result: ''}) {
+function dump(node, indent = 0, context = { result: "" }) {
   const type = node.type;
   const desc =
     type === "Root" ? "" : type === "Element" ? node.tag : node.content;
 
-  context.result += `${"-".repeat(indent)}${type}: ${desc}\n`
+  context.result += `${"-".repeat(indent)}${type}: ${desc} ${
+    node.jsNode ? '\n jsNode:' + JSON.stringify(node.jsNode, null, indent + 2) : ""
+  }\n`;
   if (node.children) {
     node.children.forEach((n) => dump(n, indent + 2, context));
   }
-  return context.result
+  return context.result;
 }
 
 const ast = parse(`<div><p>Vue</p><p>Template</p></div>`);
 dump(ast);
 
-
 function traverseNode(ast, context) {
-  context.currentNode = ast
-  const exitFns = []
-  const transforms = context.nodeTransforms
-  
-  for (let i =0; i<transforms.length; i ++) {
-    const onExit = transforms[i](context.currentNode, context)
+  context.currentNode = ast;
+  const exitFns = [];
+  const transforms = context.nodeTransforms;
+
+  for (let i = 0; i < transforms.length; i++) {
+    const onExit = transforms[i](context.currentNode, context);
     if (onExit) {
-      exitFns.push(onExit)
+      exitFns.push(onExit);
     }
-    if (!context.currentNode) return
+    if (!context.currentNode) return;
   }
 
   const children = context.currentNode.children;
   if (children) {
-    for (let i =0; i<children.length; i++) {
-      context.parent = context.currentNode
+    for (let i = 0; i < children.length; i++) {
+      context.parent = context.currentNode;
       context.childIndex = i;
-      traverseNode(children[i], context)
+      traverseNode(children[i], context);
     }
   }
 
   let i = exitFns.length;
-  while(i --) {
+  while (i--) {
     exitFns[i]();
   }
 }
+function createStringLiteral(value) {
+  return {
+    type: "StringLiteral",
+    value,
+  };
+}
+
+function createIdentifier(name) {
+  return {
+    type: "Identifier",
+    name,
+  };
+}
+
+function createArrayExpression(elements) {
+  return {
+    type: "ArrayExpression",
+    elements,
+  };
+}
+
+function createCallExpression(callee, args) {
+  return {
+    type: "CallExpression",
+    callee: createIdentifier(callee),
+    arguments: args,
+  };
+}
 
 function transformElement(node) {
-  if (node.type === 'Element' && node.tag === 'p') {
-    node.tag = 'h1'
+  return () => {
+    if (node.type !== "Element") return 
+
+    const callExp = createCallExpression('h', [createStringLiteral(node.tag)])
+    node.children === 1 ? 
+      callExp.arguments.push(node.children[0].jsNode)
+      : callExp.arguments.push(createArrayExpression(node.children.map(c => c.jsNode)))
+    node.jsNode = callExp
+  }
+}
+
+function transformRootNode(node) {
+  return () => {
+    if (node.type !== 'Root') return
+
+    const vnodeJSAST = node.children[0].jsNode
+    node.jsNode = {
+      type: 'FunctionDecl',
+      id: {type: 'Identifier', name: 'render'},
+      params: [],
+      body: [
+        {
+          type: 'ReturnStatement',
+          return: vnodeJSAST
+        }
+      ]
+    }
   }
 }
 
 function transformText(node, context) {
-  if (node.type === 'Text') {
-    context.replaceNode({
-      type: 'Element',
-      tag: 'span',
-      children: [
-        {
-          type: 'Element',
-          tag: 'i',
-        }
-      ]
-    })
-    context.removeNode();
+  if (node.type !== "Text") {
+    return;
   }
+  node.jsNode = createStringLiteral(node.content);
 }
+
 export function transform(ast) {
   const context = {
     currentNode: null,
     childIndex: 0,
     parent: null,
     replaceNode(node) {
-      context.parent.children[context.childIndex] = node
-      context.currentNode = node
+      context.parent.children[context.childIndex] = node;
+      context.currentNode = node;
     },
     removeNode() {
       if (context.parent) {
-        context.parent.children.splice(context.childIndex, 1)
-        context.currentNode = null
+        context.parent.children.splice(context.childIndex, 1);
+        context.currentNode = null;
       }
     },
-    nodeTransforms: [
-      transformElement,
-      transformText
-    ]
-  }
-  traverseNode(ast, context)
-  return dump(ast)
+    nodeTransforms: [transformElement, transformText, transformRootNode],
+  };
+  traverseNode(ast, context);
+  return ast
 }
