30de354463c9ec0863eeba73157a52c92a47e21b
daolanfler <daolanfler@gmail.com>
2022-04-20 23:00:52 +0800

    update

---
+++ apps/interview/pwt/04/4-2-3.ts
@@ -0,0 +1,20 @@
+declare const percentageType: unique symbol;
+
+class Percentage {
+  [percentageType]: void;
+  value: number;
+
+  private constructor(value: number) {
+    this.value = value;
+  }
+
+  static makePercentage(value: number): Percentage {
+    if (value < 0) {
+      value = 0;
+    } else if (value > 100) {
+      value = 100;
+    }
+
+    return new Percentage(value);
+  }
+}
