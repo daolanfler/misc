615459b9f571ef1d6f16c7d21da2075044085984
daolanfler <daolanfler@gmail.com>
2022-04-20 23:00:52 +0800

    update

---
+++ apps/interview/pwt/04/4-3.ts
@@ -0,0 +1,57 @@
+class Bike {
+  ride(): void {
+    //
+  }
+}
+
+class SportsCar {
+  drive(): void {
+    //
+  }
+}
+
+const myBike: Bike = new Bike();
+
+myBike.ride();
+
+// const myPretendSportsCar: SportsCar = myBike as unknown as SportsCar;
+const myPretendSportsCar: SportsCar = <SportsCar><unknown>myBike; // type cast 
+
+myPretendSportsCar.drive();
+
+
+class Either<TLeft, TRight> {
+  private readonly value: TLeft | TRight;
+  private readonly left: boolean;
+
+  private constructor(value: TLeft | TRight, left: boolean) {
+    this.value= value;
+    this.left = left;
+  }
+
+  isLeft(): boolean {
+    return this.left;
+  }
+
+  getLeft(): TLeft {
+    if (!this.isLeft()) throw new Error();
+    return this.value as TLeft;
+  }
+
+  isRight():boolean {
+    return !this.left;
+  }
+
+  getRight(): TRight {
+    if(!this.isRight()) throw new Error();
+    return this.value as TRight;
+  }
+
+  static makeLeft<TLeft, TRight>(value: TLeft) {
+    return new Either<TLeft, TRight>(value, true);
+  }
+
+  static makeRight<TLeft, TRight>(value: TRight) {
+    return new Either<TLeft, TRight>(value, false);
+  }
+}
